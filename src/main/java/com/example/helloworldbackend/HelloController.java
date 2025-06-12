4127a496fcdc05a155c49b455e92a8de18941810
Abhisek Jha <abhisekjha2020@gmail.com>
2025-06-11 21:28:22 -0500

    Update feature

--- src/main/java/com/example/helloworldbackend/HelloController.java
+++ src/main/java/com/example/helloworldbackend/HelloController.java
@@ -7,7 +7,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping("/")
-    public String hello(){
+    public String hello() {
         return "Hello from your custom Spring Boot backend!\n and this is a change";
+    }
+
+    @GetMapping("/secret")
+    public String getSecret() {
+        String password = System.getenv("DB_PASSWORD");
+        return "Secret loaded: " + (password != null ? "✅" : "❌");
     }
 }
