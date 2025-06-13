e5950191a7b63b3d426c7c4733710091ec52976a
Abhisek Jha <abhisekjha2020@gmail.com>
2025-06-13 08:14:53 -0500

    ci: add GitHub Actions pipeline

--- src/main/java/com/example/helloworldbackend/HelloController.java
+++ src/main/java/com/example/helloworldbackend/HelloController.java
@@ -2,13 +2,14 @@ package com.example.helloworldbackend;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.time.LocalDateTime;
 
 @RestController
 public class HelloController {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello from your custom Spring Boot backend and Docker!";
+        return "Hello from your custom Spring Boot backend and Docker and Time stamp is ! " + LocalDateTime.now();
     }
 
 }
